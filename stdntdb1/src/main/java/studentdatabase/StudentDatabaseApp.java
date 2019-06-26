522b90bc67b093a2884bd251f77487a9fd680c4c
jbexx <jbecks.media@gmail.com>
2019-06-26 12:41:44 -0600

    create n number of students based on user input

--- stdntdb1/src/main/java/studentdatabase/StudentDatabaseApp.java
+++ stdntdb1/src/main/java/studentdatabase/StudentDatabaseApp.java
@@ -1,10 +1,22 @@
 package studentdatabase;
+import java.util.Scanner;
 
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.toString());
+        //ask how many students to add
+        System.out.println("Enter number of students to enroll: ");
+        Scanner in = new Scanner(System.in);
+
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+        }
+        for (int n = 0; n < numOfStudents; n++) {
+            System.out.println(students[n].toString());
+        }
     }
 }
